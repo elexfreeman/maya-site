7239455d3f35578fe1e302a838ac8e5e8f57b874
elextraza <elextraza@gmail.com>
2025-12-21 23:09:47 +0300

    styles top page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,32 @@
 <template>
-  <nav class="navbar">
-    <div class="nav-container">
-      <div class="logo">
-      <a href="/">
-        <h2>Ангел Удачи</h2>
-      </a>
-      </div>
-      <ul class="nav-menu">
-        <li><a href="/#about">Обо мне</a></li>
-        <li><a href="/#myway">Мой стиль</a></li>
-        <li><a href="/#services">Услуги</a></li>
-        <li><a href="/#gallery">Магазин</a></li>
-        <li><a href="/#contact">Контакты</a></li>
-      </ul>
-      <a class="cta-button Telegram-button" href="https://t.me/angeludachi_bot" target="_blank" rel="noopener noreferrer">Связаться в Telegram</a>
-    </div>
-  </nav>
+    <nav class="navbar">
+        <div class="nav-container">
+            <div class="logo">
+                <a href="/">
+                    <h2>Ангел Удачи</h2>
+                </a>
+            </div>
+            <ul class="nav-menu">
+                <li><a href="/#about">Обо мне</a></li>
+                <li><a href="/#myway">Мой стиль</a></li>
+                <li><a href="/#services">Услуги</a></li>
+                <li><a href="/#gallery">Магазин</a></li>
+                <li><a href="/#contact">Контакты</a></li>
+            </ul>
+            <a
+                class="cta-button Telegram-button"
+                href="https://t.me/angeludachi_bot"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Связаться в Telegram</a
+            >
+            <a
+                class="cta-button small Telegram-button"
+                href="https://t.me/angeludachi_bot"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Связаться</a
+            >
+        </div>
+    </nav>
 </template>
